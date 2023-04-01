3c9db9e0d5fd607ad99d5b89edb34e3465c485a1
UnderWaterWelder27 <vvlad1175@gmail.com>
2023-04-01 21:16:52 +0300

    renamed sequence coefficients variables

--- Lection_1/Task_4/main.py
+++ Lection_1/Task_4/main.py
@@ -1,21 +1,24 @@
 from math import *
 
 # a*x^2 + b*x + c = 0
+# a - senior coefficient
+# b - coefficient at x
+# c - free member
 
-a = float(input("Enter variable (a) >> "))
-b = float(input("Enter variable (b) >> "))
-c = float(input("Enter variable (c) >> "))
-discriminator = pow(b, 2) - 4 * a * c
+senior_coefficient = float(input("Enter variable (a) >> "))
+coefficient_at_x = float(input("Enter variable (b) >> "))
+free_member = float(input("Enter variable (c) >> "))
+discriminator = pow(coefficient_at_x, 2) - 4 * senior_coefficient * free_member
 
 print(f"Discriminator = {discriminator}")
 
-if a == 0:
-    print(f"Variable \'a\' must not be zero ! But... \n " f"x = {-c / b}")
+if senior_coefficient == 0:
+    print(f"Variable \'a\' must not be zero ! But... \n " f"x = {-free_member / coefficient_at_x}")
 elif discriminator < 0:
     print("There is no solution ((")
 elif discriminator == 0:
-    print(f"There is one root \n x = {-b / (2 * a)}")
+    print(f"There is one root \n x = {-coefficient_at_x / (2 * senior_coefficient)}")
 else:
     print(f"There is two roots \n "
-          f"x1 = {(-b + sqrt(discriminator)) / (2 * a)} \n "
-          f"x2 = {(-b - sqrt(discriminator)) / (2 * a)}")
+          f"x1 = {(-coefficient_at_x + sqrt(discriminator)) / (2 * senior_coefficient)} \n "
+          f"x2 = {(-coefficient_at_x - sqrt(discriminator)) / (2 * senior_coefficient)}")

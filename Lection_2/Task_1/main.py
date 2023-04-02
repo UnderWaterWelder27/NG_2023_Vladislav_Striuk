d1607d17b9e4b5dd0f0a4ebd25a9ae06c4aeb488
UnderWaterWelder27 <vvlad1175@gmail.com>
2023-04-02 13:06:47 +0300

    created function to count list elements

---
+++ Lection_2/Task_1/main.py
@@ -0,0 +1,4 @@
+
+chaotic_list = [chaotic_list for chaotic_list in input("\n Enter elements and separate them by a space: ").split()]
+print(f"\n Elements: {chaotic_list}",
+      f"\n Number of elements: {len(chaotic_list)}")

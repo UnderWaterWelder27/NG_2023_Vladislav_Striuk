415c8c7020dc765b1e4fd2113ec0b9273ba2bc40
UnderWaterWelder27 <vvlad1175@gmail.com>
2023-04-11 10:04:23 +0300

    simplified match-case operation | print result command separated

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -1,16 +1,19 @@
 first_operand = float(input("1st operand >> "))
 second_operand = float(input("2st operand >> "))
+result = float()
 
-print("Press 1-4 numeric button to confirm operation: \n"
-      "(1) --- Add (+) \n"
-      "(2) --- Subtract (-) \n"
-      "(3) --- Multiply (*) \n"
-      "(4) --- Divide (/) \n")
+print("Press [+], [-], [*], [/] button to confirm operation: \n"
+      "[+] --- Add \n"
+      "[-] --- Subtract \n"
+      "[*] --- Multiply \n"
+      "[/] --- Divide \n")
 
-operation = int(input(">> "))
+operation = input(">> ")
 match operation:
-    case 1: print("{0} + {1} = ".format(first_operand, second_operand), str(first_operand + second_operand))
-    case 2: print("{0} - {1} = ".format(first_operand, second_operand), str(first_operand - second_operand))
-    case 3: print("{0} * {1} = ".format(first_operand, second_operand), str(first_operand * second_operand))
-    case 4: print("{0} / {1} = ".format(first_operand, second_operand), str(first_operand / second_operand))
+    case '+': result = first_operand + second_operand
+    case '-': result = first_operand - second_operand
+    case '*': result = first_operand * second_operand
+    case '/': result = first_operand / second_operand
     case _: print("Idk what to do with this numbers, so, just look at them: {0}, {1} ")
+
+print(f"{first_operand} {operation} {second_operand} = {result}")
